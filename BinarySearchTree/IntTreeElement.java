2ce902164212661bc0beedde98950c1be4a7f634
midNight-jam <malviya.jayam@gmail.com>
2016-08-22 23:18:09 -0700

    adding the comment about the implementation of ItreeElement

--- BinarySearchTree/IntTreeElement.java
+++ BinarySearchTree/IntTreeElement.java
@@ -2,6 +2,9 @@ package com.darkRealm.BST;
 
 /**
  * Created by Jayam on 8/21/2016.
+ * An example of a Type being the TreeElement, for simplicity this class is having direct data of integer. 
+ * More complex classes can have their behaviour & implement the ITreeElement to decide which 
+ * part/combination of state to account for data.
  */
 public class IntTreeElement implements ITreeElement {
     IntTreeElement _left;
