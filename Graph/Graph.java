fab01c14afd9d8eec46ecd0464855fac855b899a
Jayam Malviya <malviya.jayam@gmail.com>
2016-08-28 13:21:17 -0600

    Adding the BFS traversal functionality to Graph.

--- Graph/Graph.java
+++ Graph/Graph.java
@@ -1,5 +1,8 @@
 package com.darkRealm.Graph;
 
+import com.darkRealm.Queue.IQueueElement;
+import com.darkRealm.Queue.IntQueElement;
+import com.darkRealm.Queue.Queue;
 import com.darkRealm.Stack.Stack;
 
 /**
@@ -25,6 +28,7 @@ public class Graph implements IGraph {
 
     @Override
     public void doDepthFirst() {
+        resetVeterxStatuses(); // reset Statuses
         //push first vertex in stack & make its status as pushed
         Vertex vertex = _vertices[0];
         vertex.status = Status.Pushed;
@@ -34,15 +38,17 @@ public class Graph implements IGraph {
         while (!stack.isEmpty()) {
             vertex = (Vertex) stack.pop();
             vertex.status = Status.Processed;
-            // this poped vertex is the vertex as per the DFS
+            // this poped vertex is the vertex as per the DFS traversal
             System.out.println(vertex._vertexNo);
             for (int i = 0; i < _noOfVertices; i++) {
-                // as you pop the vertex push the next adjacent/reachable vertex in the stack & mark its status as pushed
-                if ((_adjacencyMatrix[vertex._vertexNo][i] == 1) && _vertices[i].status == Status.NotProcessed) {
+                // as you pop the vertex,
+                // push the next adjacent/reachable notprocessed vertex in the stack & mark its status as pushed
+                boolean adjacentVertexButNotProcessed =
+                        _adjacencyMatrix[(int) vertex.getData()][i] == 1 && _vertices[i].status == Status.NotProcessed;
+                if (adjacentVertexButNotProcessed) {
                     Vertex v2 = _vertices[i];
                     v2.status = Status.Pushed;
                     stack.push(v2);
-//                    break;
                 }
             }
         }
@@ -50,6 +56,36 @@ public class Graph implements IGraph {
 
     @Override
     public void doBreadthFirst() {
+        resetVeterxStatuses();
+        Queue queue = new Queue<IntQueElement>(_noOfVertices);
+        Vertex vertex = _vertices[0];
+        // insert first vertex & make its status as pushed
+        vertex.status = Status.Pushed;
+        queue.insert(vertex);
+        while (!queue.isEmpty()) {
+            // this removed vertex is the vertex as per the BFS traversal
+            vertex = (Vertex) queue.remove();
+            vertex.status = Status.Processed;
+            System.out.print(" " + vertex.getData());
+            for (int i = 0; i < _noOfVertices; i++) {
+                // as you remove the vertex,
+                // insert the next adjacent/reachable not processed vertex in the queue & mark its status as pushed
+                boolean adjacentVertexButNotProcessed =
+                        _adjacencyMatrix[(int) vertex.getData()][i] == 1 && _vertices[i].status == Status.NotProcessed;
+                if (adjacentVertexButNotProcessed) {
+                    Vertex v2 = _vertices[i];
+                    v2.status = Status.Pushed;
+                    queue.insert(v2);
+                }
+            }
+        }
 
+    }
+
+    @Override
+    public void resetVeterxStatuses() {
+        for (int i = 0; i < _noOfVertices; i++) {
+            _vertices[i].status = Status.NotProcessed;
+        }
     }
 }
